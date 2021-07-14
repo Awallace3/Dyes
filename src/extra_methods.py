ab845fc16e2261488fccbd22576528614daa9845
Austin Wallace <austinwallace196@gmail.com>
2021-07-14 14:42:38 -0500

    extra_methods gather function and delete function in monitor_jobs_collect

--- src/extra_methods.py
+++ src/extra_methods.py
@@ -1,6 +1,82 @@
 import os
 import glob
 import error_mexc_dyes_v1
+import subprocess
+import pandas
+import numpy as np
+
+def gather_excitation_data(path, output_path, add_methods, outName= 'mexc_o', chem_package='Gaussian', moleculeName="", nStates=3):
+	add_methods_length = len(add_methods['methods'])
+	if add_methods_length != len(add_methods['basis_sets']) and add_methods_length != len(add_methods['mem_com']) and add_methods_length != len(add_methods['mem_pbs']) and add_methods_length != len(add_methods['solvents']):
+		print("add_methods must have values that have lists of the same length.\nTerminating jobResubmit before start")
+		return
+	os.chdir(path)
+	data_dict = {}
+	df = {
+		'Name': ["%s Exc %d" % (moleculeName, i) for i in range(1, nStates+1) ]
+	}
+	df = pandas.DataFrame(df)
+	for i in range(len(add_methods['methods'])):
+		if add_methods['solvents'] != '':
+			dir_name = ('%s_%s_%s' % (add_methods['methods'][i], add_methods['basis_sets'][i].replace("(", "").replace(")", ""), add_methods['solvents'][i])).lower()
+			header_name = ('%s/%s in %s' % (add_methods['methods'][i], add_methods['basis_sets'][i], add_methods['solvents'][i]))
+		else:
+			dir_name = ('%s_%s' % (add_methods['methods'][i], add_methods['basis_sets'][i].replace("(", "").replace(")", "") )).lower()
+			header_name = ('%s/%s' % (add_methods['methods'][i], add_methods['basis_sets'][i] ))
+		if not os.path.exists(dir_name):
+			print('No directory exists: %s\n moving to next method' % dir_name)
+		else:
+			
+
+
+			if chem_package == 'Gaussian':			
+				outFile = glob.glob("%s/*.out" % dir_name)
+				print()
+				print(outFile[-1])
+				if len(outFile) > 0:
+					cmd = "awk '/Excited State/ {print $5, $9}' %s | tac | tail -n %d | tac | sed s/f=// > tmp.csv" % (outFile[-1], nStates)
+					subprocess.call(cmd, shell=True)
+					#with open('tmp.csv', 'r') as fp:
+						#data = fp.read().split("\n")[:-1]
+						#data = [ [float(j[0]), float(j[1])] for j in [i.split(" ") for i in data]]
+						#print(data)
+					data = np.genfromtxt('tmp.csv', delimiter=" ")
+					print(data)
+					#data_dict[dir_name] = data
+					if len(data) > 0:
+						eV = data[:,0]
+						osc = data[:,1]
+						if add_methods['solvents'][i] != '':
+							header_name = ('%s/%s in %s' % (add_methods['methods'][i], add_methods['basis_sets'][i], add_methods['solvents'][i]))
+						else:
+							header_name = ('%s/%s' % (add_methods['methods'][i], add_methods['basis_sets'][i] ))
+						
+						header = "%s Excitation Energy (eV)" % (header_name)
+						df[header] = eV
+						header = "%s Oscillator Strength" % (header_name)
+						df[header] = osc
+	df.to_csv('%s/tmp.csv' % output_path)
+	"""
+	Name	M/BS 	M/BS ... 
+	xyz		exc1	exc1 ...
+	xyz		exc2	exc2 ...
+	xyz		exc3	exc3 ...
+	"""
+
+	return data_dict
+
+def data_dict_to_pandas(data_dict, ):
+	df = {
+		"Name" : [],
+		"Exc1" : [],
+		"Exc2" : [],
+		"Exc3" : [],	
+	}
+
+				
+
+   	
+
 
 def specifc_file_gen(path, add_methods, cluster='seq', outName='mexc_o', baseName='mexc', chem_package='Gaussian'):
 	add_methods_length = len(add_methods['methods'])
@@ -79,9 +155,11 @@ def main():
 		"mem_pbs" : ["10", "10", "10", "10", "10", "10" , "10", "10", "10", "10" ],
 	}
 	path = '../testing_results/test_functionals/AP25/b3lyp/new_geom'
-	#path = '../testing_results/test_functionals/XY1/GO'
+	path = '../testing_results/test_functionals/XY1/GO'
 	
-	specifc_file_gen(path, add_methods, chem_package="Gaussian")
+	#specifc_file_gen(path, add_methods, chem_package="Gaussian")
+	output_path = '~/research/Dyes/testing_results/'
+	gather_excitation_data(path, output_path, add_methods, moleculeName="XY1")
 
 	add_methods = {
 		"methods" : [ "CC2" ],
