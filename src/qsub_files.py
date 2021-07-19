8897a45eb02edfd1af34468baf9c254b79234835
Austin Wallace <austinwallace196@gmail.com>
2021-07-19 10:14:39 -0500

    fix mex edges 3

--- src/qsub_files.py
+++ src/qsub_files.py
@@ -41,6 +41,31 @@ def fix_broken(resubmit, path_results='../results'):
 
         os.chdir("..")
 
+def fix_mex(resubmit):
+    os.chdir("../results")
+    
+    for i in resubmit:
+        print(i)
+        os.chdir(i)
+        out_files = glob.glob("*.out*")
+        out_completion = glob.glob("*_o*")
+        len_file = len(out_files)
+        len_complete = len(out_completion)
+        #print("len_complete", len(out_completion), "len_outfiles", len(out_files))
+        if len_complete > len_file:
+            for i in range(len_complete-len_file):
+                del_o = out_completion.pop()
+                #subprocess.call("echo " + del_o, shell=True)
+                print('rm %s' % del_o)
+                subprocess.call("rm " + del_o, shell=True)
+        elif len_complete < len_file:
+            for i in range((len_file - len_complete)):
+                create_o = "mex_o.o%s0000" % str(i+15)
+                print('touch %s' % create_o)
+                #subprocess.call("echo " + create_o, shell=True)
+                subprocess.call("touch " + create_o, shell=True)
+        os.chdir("..")
+
 """
 "TPA2_4b_2ea",
 "7ed_6b_3ea",
@@ -131,4 +156,5 @@ resubmit = [
     "TPA2_15b_2ea",]
 #broken_resubmit('../results')
 
-fix_broken(resubmit)
+#fix_broken(resubmit)
+fix_mex(resubmit)
