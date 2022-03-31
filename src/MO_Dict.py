62283f4cf68b2589099f6864ca0284e719db225c
tsantaloci <tsantalo@go.olemiss.edu>
2022-03-31 10:42:25 -0500

    Taylor edits

---
+++ src/MO_Dict.py
@@ -0,0 +1,388 @@
+import os
+
+def amountofbasisfunctions(file):
+    filename = open(file,'r')
+    data =  filename.readlines()   
+    aa = 0
+    for num,line in enumerate(data):
+        if 'basis functions,' in line: 
+            line = line.split(',')
+            line = line[0].replace('basis functions','')
+            aa += int(line)
+
+           # print(int(line))
+
+    return aa
+
+def orbital_sym(file):
+    filename = open(file,'r')
+    data = filename.readlines()
+    for num,line in enumerate(data):
+        if 'Orbital symmetries:' in line:
+            print()
+
+    return
+
+def atom_num_let_dic(file):
+    filename = open(file,'r')
+    data = filename.readlines()
+    coord = {}
+    first_num = []
+    last_num = []
+    for num,i in enumerate(data):
+        if 'Standard orientation:' in i:
+            first_num.append(num+5)
+           # print(data[num+5])
+        if ' ---------------------------------------------------------------------' in i:
+            last_num.append(num)
+    for line in data[int(first_num[0]):int(last_num[-1])]:
+        line = line.strip().replace("  ", " ").replace("  ", " ").replace("  ", " ").replace("  ", " ").replace("  ", " ")
+        line = line.split(" ") 
+        if line[1]=='7':
+            let='N'
+            coord[line[0]]=let
+        if line[1]=='6':
+            let='C'
+            coord[line[0]]=let
+        if line[1]=='1':
+            let='H'
+            coord[line[0]]=let
+        if line[1] == '8':
+            let='O'
+            coord[line[0]]=let
+        if line[1]=='16':
+            let='S'
+            coord[line[0]]=let
+        if line[1]=='9':
+            let='F'
+            coord[line[0]]=let
+        if line[1]=='14':
+            let='Si'
+            coord[line[0]]=let
+    #print(coord)
+    return coord
+
+
+def lastO(file,basis):
+    filename = open(file,'r')
+    data = filename.readlines()
+    
+    number = []
+    for num,line in enumerate(data):
+        if 'Molecular Orbital Coefficients:' in line:
+            number.append(num)
+
+        if '     Density Matrix:' in line:
+            number.append(num)
+    #print(data[number[0]:number[1]])
+    #print(number)
+
+    last_num = 0
+    for num,line in enumerate(data[number[0]:number[1]]):
+        #print(line)
+        line = line.replace('\n','')
+        #print(line)
+        if '        O        ' in line:
+        #    line = line.strip(' ')
+        #    line=line.split('         ')
+           # print(num)
+           if 'V' in line:
+            
+
+                line = line.strip().replace("  ", " ").replace("  ", " ").replace("  ", " ").replace("  ", " ").replace("  ", " ")
+                line = line.split(" ")
+                occ = -1
+                print((occ,'start'))
+ 
+                #if line[i][-1] == "V":
+                
+                vir = 0
+                
+                for i in range(-1, -len(line), -1):
+                    if line[0]=="O" and line[1]=="O" and line[2]=="O" and line[3]=="O" and line[4]=="V":
+                        vir = -1
+                        occ = -1 - 1 
+                    if line[0]=="O" and line[1]=="O" and line[2]=="O" and line[3]=="V" and line[4]=="V": 
+                        vir = -2
+                        occ = -3
+                    if line[0]=="O" and line[1]=="O" and line[2]=="V" and line[3]=="V" and line[4]=="V": 
+                        vir = -3
+                        occ = -4
+                    if line[0]=="O" and line[1]=="V" and line[2]=="V" and line[3]=="V" and line[4]=="V": 
+                        vir = -4
+                        occ = -5
+    
+               # print((occ,'done'))
+                #vir = occ + 1
+               # print(occ, vir)
+                last_num = num+number[0]+1
+                start_homo = last_num+1
+                end_homo = last_num+basis+1 
+                start_lumo = last_num+1
+                end_lumo =  last_num+basis+1 
+                print(line)
+                for i in line:
+                    i
+           else:
+                #print(line)
+                #print(num+number[0])
+                start_homo = num+number[0]+1 
+                #print((start_homo,'Start HOMO'))
+                end_homo = start_homo+last_num+basis+1 
+                #print((end_homo,'End HOMO'))
+                start_lumo = start_homo+ last_num+basis+3
+              #  print((start_lumo,'Start LUMO'))
+                end_lumo = start_lumo+ last_num+basis+1
+              #  print((end_lumo,'End LUMO'))
+           # print("virtual in next block")
+                occ = -1
+                vir = -5
+
+       
+  
+    #return tot_homo,tot_lumo
+    return start_homo,end_homo,start_lumo,end_lumo,occ,vir
+
+
+def homo_dict(file,start_num,end_num,occ):
+    filename = open(file,'r')
+    data = filename.readlines()
+    tot_homo = {
+
+    }
+    for num,i in enumerate(data[start_num:end_num]):
+        #print(i)
+        if 'Eigenvalues' in i:
+            i
+        else:
+            #print(i[occ])
+            i = i.replace("  ", " ").replace("  ", " ").replace("  ", " ").replace("  ", " ").replace("  ", " ").replace("  ", " ")
+            i = i.split(" ")[1:]
+            #print(i)
+           # atom = {}
+            if len(i) == 9:
+                #print(i, len(i),num+last_num)
+                #print(i[1])
+                #atom['type'.update(i[1])
+                type1 = i[1]
+                #atom['type'].update(type1)
+                '''
+                atom['occ_coef'].update(float(i[occ]))
+                occup={'occ_coef':float(i[occ]}
+                
+                atom['vir_coef'].update(float(i[vir])**2)
+                atom['line_num'].update(num +last_num)
+                '''
+                
+                
+                atom = {
+                    'type': i[1],
+                    'occ_coef': [float(i[occ])**2],
+              #      'vir_coef': [float(i[vir])**2],
+              #      'line_num': num+last_num
+                }
+                #print(atom['line_num'])
+            else:
+                atom['occ_coef'].append(float(i[occ])**2)
+               # atom['vir_coef'].append(float(i[vir])**2)
+                tot_homo[atom['type']]=atom['occ_coef']
+               # tot_lumo[atom['type']]=atom['vir_coef']
+    print(tot_homo['1'])
+
+    return tot_homo
+
+def lumo_dict(file,start_num,end_num,vir):
+    filename = open(file,'r')
+    data = filename.readlines()
+    tot_lumo = {
+
+    }
+    for num,i in enumerate(data[start_num:end_num]):
+        #print(i)
+        if 'Eigenvalues' in i:
+            i
+        else:
+            #print(i[occ])
+            i = i.replace("  ", " ").replace("  ", " ").replace("  ", " ").replace("  ", " ").replace("  ", " ").replace("  ", " ")
+            i = i.split(" ")[1:]
+            #print(i)
+           # atom = {}
+            if len(i) == 9:
+                #print(i, len(i),num+last_num)
+                #print(i[1])
+                #atom['type'.update(i[1])
+                type1 = i[1]
+                #atom['type'].update(type1)
+                '''
+                atom['occ_coef'].update(float(i[occ]))
+                occup={'occ_coef':float(i[occ]}
+                
+                atom['vir_coef'].update(float(i[vir])**2)
+                atom['line_num'].update(num +last_num)
+                '''
+                
+                
+                atom = {
+                    'type': i[1],
+                    'vir_coef': [float(i[vir])**2],
+                }
+            else:
+                atom['vir_coef'].append(float(i[vir])**2)
+                tot_lumo[atom['type']]=atom['vir_coef']
+    print(tot_lumo['1'])
+
+    return tot_lumo
+
+
+
+
+
+
+
+
+
+
+def summer(dict_homo,dict_lumo,num):
+    print(num)
+    final_homo = {}
+    final_lumo = {}
+
+
+    for atom_num in num.keys():
+        #print(dict_homo[atom_num])
+        homo_tot = {
+        'atom_lett':{},
+        'total':{}
+        }
+        tot_homo = sum(dict_homo[atom_num])
+        homo_tot['atom_lett']=num[atom_num]
+        homo_tot['total']=tot_homo
+       # final.update(homo_tot)
+        final_homo[atom_num]= homo_tot
+
+        lumo_tot = {
+        'atom_lett':{},
+        'total':{}
+        }
+        tot_lumo = sum(dict_lumo[atom_num])
+        lumo_tot['atom_lett']=num[atom_num]
+        lumo_tot['total']=tot_lumo
+       # final.update(homo_tot)
+        final_lumo[atom_num]= lumo_tot
+
+       # print(homo_tot)
+    '''   
+    print(final_homo)
+
+    print(final_homo['1']['atom_lett'])    
+    print(final_homo['1']['total'])
+    print(final_lumo['1']['atom_lett'])
+    print(final_lumo)
+    '''
+
+    #dict_homo
+    
+    return final_homo,final_lumo
+
+def total_contrib(dict_homo,dict_lumo,num):
+    tot_homo = 0
+    tot_lumo = 0
+    for atom_num in num.keys():
+        #print(atom_num)
+        tot_homo += dict_homo[atom_num]['total']
+        tot_lumo += dict_lumo[atom_num]['total']
+    print(tot_homo)
+    print(tot_lumo)
+    
+    return tot_homo,tot_lumo
+
+def part_tot_contrib(dict_homo,dict_lumo,num,tot_homo,tot_lumo):
+    par_homo = 0
+    par_lumo = 0
+    #atom_num_list=[33,34,35,36,37,44] 
+    for atom_num in range(33,37):
+    #for atom_num in atom_num_list:
+        print(atom_num)
+        
+        par_homo += dict_homo[str(atom_num)]['total']
+        par_lumo += dict_lumo[str(atom_num)]['total']
+        
+    #print(tot_homo)
+    
+    per_homo = par_homo/tot_homo*100
+    per_lumo = par_lumo/tot_lumo*100
+    #print(per_homo)
+    #print()
+    
+    print('The percentage of HOMO ' + str(per_homo))
+    print('The percentage of LUMO ' + str(per_lumo))
+
+    #print(tot_lumo)
+
+
+    return
+
+def Smiles_Reader(SMILES):
+    filename = open(SMILES,'r')
+    data = filename.readlines()
+    acceptor = ''
+    backbone = ''
+    donor = ''
+    for line in data:
+        line = line.split('.')
+        acceptor = line[0]
+        backbone = line[1]
+        donor = line[2]
+
+        
+
+
+    return
+
+
+
+
+
+
+
+def main():
+#    filename = '../MO_start/6ed_28b_4ea/mo/6ed_28b_4ea.out'
+#    filename = '../MO_start/3ed_7b_2ea/mo/3ed_7b_2ea.out'
+    #filename = '../MO_start/11ed_28b_8ea/mo/11ed_28b_8ea.out'
+  #  filename = '../MO_start/3ed_26b_4ea/mo/3ed_26b_4ea.out'
+  #  filename = '../MO_start/11ed_8b_8ea/mo/test/mani.out'
+  #  filename = '../MO_start/11ed_8b_8ea/mo/11ed_8b_8ea.out'
+    #Smiles = '../MO_start/11ed_8b_8ea/mo/test.smi'
+    #input_file = '../MO_start/3ed_7b_2ea/mo/3ed_7b_2ea.com'
+   # filename = '../MO_start/1ed_20b_4ea/mo/1ed_20b_4ea.out'
+    filename = '../MO_start/10ed_29b_10ea/mo/10ed_29b_10ea.out'
+
+    num = amountofbasisfunctions(filename)
+    atom_let = atom_num_let_dic(filename)
+    #print(atom_let)
+    #Smiles_Reader(Smiles)
+
+
+
+    #homo,lumo = lastO(filename,num)
+    start_num_h,end_num_h,start_num_l,end_num_l,occ,vir  = lastO(filename,num) 
+    
+    homo = homo_dict(filename,start_num_h,end_num_h,occ)
+    lumo = lumo_dict(filename,start_num_l,end_num_l,vir)
+    
+    
+    #print(homo.keys())
+    
+    
+    final_homo,final_lumo = summer(homo,lumo,atom_let)
+    tot_homo,tot_lumo = total_contrib(final_homo,final_lumo,atom_let)
+    part_tot_contrib(final_homo,final_lumo,atom_let,tot_homo,tot_lumo)
+    
+    
+
+
+
+  #  for line in lastO(filename,num):
+  #      line 
+    return
+main()
--- src/PiecesHOMOLUMOgrabber.py
+++ src/PiecesHOMOLUMOgrabber.py
@@ -2,24 +2,28 @@ import os
 import pandas as pd
 import numpy as np
 
-def grepper(name):
-    cmd = 'grep -A1 ' + "'Alpha  occ. eigenvalues' " + str(name) +  '/mex.out' + '>' + str(name)+  '/' + 'orbs.out'
+def grepper(name,meth):
+    cmd = 'grep -A1 ' + "'Alpha  occ. eigenvalues' " + str(name)+'/'+str(meth) +  '/mex.out' + '>' + str(name)+  '/'+str(meth)  + '/orbs.out'
     #print(cmd)
     os.system(cmd)
 
 
     return
 
-def LUMO(name):
-    filename = open(str(name)+ '/' + 'orbs.out','r')
+def LUMO(name,meth):
+    filename = open(str(name)+ '/' + str(meth) + '/'+ 'orbs.out','r')
     data = filename.readlines()
     lll = data[-1].split(' ')
+    meth_dict = {}
     #print(lll)
     if lll[7] == '':
      # print(lll[8])
       LUMO = float(lll[8])*27.2114
+      meth_dict[name]=LUMO
     else:
       LUMO = float(lll[7])*27.2114
+      meth_dict[name]=LUMO
+
    # LUMO = abs(LUMO)-3.9
     #print((name,'LUMO',method,LUMO))
   #  for i in filename:
@@ -27,20 +31,23 @@ def LUMO(name):
 
 
 
-    return [name,'LUMO',str(LUMO)]
+    return meth_dict 
 
-def HOMO(name):
-    filename = open(str(name)+ '/' + 'orbs.out','r')
+def HOMO(name,meth):
+    filename = open(str(name)+ '/'+ str(meth)+ '/' + 'orbs.out','r')
     data = filename.readlines()
-    print(data[-2])
+    meth_dict = {}
+    name_dict = {}
+  #  print(data[-2])
   #  print(lll)
     lll = data[-2].split(' ')
+    
     #print(lll)
     HOMO = float(lll[-1])*27.2114
+    meth_dict[name]=HOMO
+    print(meth_dict)
   #  HOMO = abs(HOMO)-4.2
-    #print((name,'HOMO',method,HOMO))
-
-    return [name,'HOMO',str(HOMO)] 
+    return meth_dict
 
 def pandass(HOMO,LUMO):
     hom = np.array(HOMO)
@@ -48,24 +55,28 @@ def pandass(HOMO,LUMO):
 
     
     #print(HOMO)
-    df =  pd.DataFrame(hom,columns=['Name','Type','Energy (eV)'])
+    df =  pd.DataFrame(hom,columns=['Name','Type','Energy (eV)','Method'])
     print(df)
-    df_2 = pd.DataFrame(lum,columns=['Name','Type','Energy (eV)']) 
+    df_2 = pd.DataFrame(lum,columns=['Name','Type','Energy (eV)','Method']) 
     print(df_2)
     HOMOenerg = []
     HOMOname = []
+    HOMOmethod = []
     LUMOname = []
     LUMOenerg = []
+    LUMOmethod = []
     for i in HOMO:
       HOMOname.append(i[0])
       HOMOenerg.append(round(float(i[2]),4))
+      HOMOmethod.append(i[3])
     for x in LUMO:
       LUMOname.append(x[0])
       LUMOenerg.append(round(float(x[2]),4))
+      LUMOmethod.append(x[3])
 
-    d = {'Name':HOMOname,'HOMO': HOMOenerg,'LUMO:':LUMOenerg}
+    d = {'Name':HOMOname,'HOMO': HOMOenerg,'LUMO:':LUMOenerg,'Method':HOMOmethod}
     df_3 = pd.DataFrame(data=d)
-    df_3.to_csv('data.csv',index=False)
+    df_3.to_csv('/Users/tsantaloci/Desktop/python_projects/austin/Dyes/data_analysis/Pieces.csv',index=False)
 
 
 
@@ -73,31 +84,61 @@ def pandass(HOMO,LUMO):
     return
 
 def Main():
-    path_to_files = '../DFTpieces/eAcceptor/'
-    path_to_files = '../DFTpieces/eDonor/'
+  #  path_to_files = '../DFTpieces/eAcceptor/'
+  #  path_to_files = '../DFTpieces/eDonor/'
     path_to_files = '../DFTpieces/Backbone/'
     os.chdir(path_to_files)
     #name = ['NL2','AP25','JW1','R4']
     # method = ['mexc','mexc_dichloromethane','pbe1pbe','pbe1pbe_dichloromethane']
     # name =  ['DQ5', 'S-DAHTDTT', 'NKX-2883', 'S3', 'HKK-BTZ4', 'TPA-T-TTAR-A', 'NL7', 'NL8', 'AP3', 'NL11', 'C271',  'IQ4', 'WS-55', 'SGT-130', 'FNE52', 'IQ21', 'SGT-136', 'D-DAHTDTT', 'R6', 'TPA-TTAR-A', 'T-DAHTDTT', 'TH304', 'NL4', 'C258', 'TTAR-9', 'SGT-121', 'TTAR-15', 'NL2', 'SGT-129', 'FNE32', 'BTD-1', 'Y123', 'C272', 'FNE34', 'IQ6', 'TP1', 'TTAR-B8', 'R4', 'TPA-T-TTAR-T-A','ZL003','WS-6','NL4','NL6','JW1','AP25']
-   # methods = ['mexc', 'pbe1pbe','bhandlyp']
-    name = ['10ea','11ea','1ea','2ea','3ea','4ea','5ea','6ea','7ea','8ea','9ea']   
-    name = ['10ed','11ed','12ed','1ed','2ed','3ed','5ed','6ed','7ed','9ed']
+    methods = ['CAM-B3LYP', 'PBE1PBE','bhandhlyp']
+  #  name = ['10ea','11ea','1ea','2ea','3ea','4ea','5ea','6ea','7ea','8ea','9ea']   
+  #  name = ['10ed','11ed','12ed','1ed','2ed','3ed','5ed','6ed','7ed','9ed']
     ## Full backbone list ##
     name = ['4b', '1b', '32b', '3b', '24b', '27b', '22b', '2b', '14b', '17b', '12b', '9b', '31b', '34b', '8b', '26b', '21b', '29b', '7b', '16b', '11b', '19b', '35b', '30b', '33b', '6b', '25b', '20b', '23b', '5b', '15b', '10b', '18b', '13b']
 
-    name = ['4b', '1b', '32b', '3b', '24b',  '22b', '2b', '14b', '17b', '12b', '9b', '31b', '34b', '8b',  '21b', '29b', '7b', '16b', '11b', '19b', '35b', '30b', '33b', '6b', '25b', '20b', '23b', '5b', '15b', '10b', '18b', '13b']
+#    name = ['4b', '1b', '32b', '3b', '24b',  '22b', '2b', '14b', '17b', '12b', '9b', '31b', '34b', '8b',  '21b', '29b', '7b', '16b', '11b', '19b', '35b', '30b', '33b', '6b', '25b', '20b', '23b', '5b', '15b', '10b', '18b', '13b']
    # method = ['mexc','mexc_dichloromethane',]
 
-    HOMO_1 = []
+    HOMO_1 = {}
     LUMO_1 = []
+    camhomo = {}
+    camlumo = {}
+    bhandhomo = {}
+    bhandlumo = {}
+    pbepbehomo = {}
+    pbepbelumo = {}
     for bench in name:
-      #  for meth in methods:
-        print(bench)
-        grepper(bench)
-        HOMO_1.append(HOMO(bench))
-        LUMO_1.append(LUMO(bench))
-    pandass(HOMO_1,LUMO_1)
+        for meth in methods:
+          print(bench)
+          grepper(bench,meth)
+          if meth == 'CAM-B3LYP':
+            camhomo.update(HOMO(bench,meth))
+            camlumo.update(LUMO(bench,meth))
+          if meth == 'PBE1PBE':
+            pbepbehomo.update(HOMO(bench,meth))
+            pbepbelumo.update(LUMO(bench,meth))
+          if meth == 'bhandhlyp':
+            bhandhomo.update(HOMO(bench,meth))
+            bhandlumo.update(LUMO(bench,meth)) 
+
+    print(camhomo.keys())
+    print(camhomo)
+    print(camlumo)
+    print(bhandhomo)
+    print(bhandlumo)
+    print(pbepbehomo)
+    print(pbepbelumo)
+    df = {'Name':camhomo.keys(),'CAM-B3LYP':camhomo.values(),'BHandHYLP':bhandhomo.values(),'PBE1PBE':pbepbehomo.values()}
+    df_2 = {'Name':camlumo.keys(),'CAM-B3LYP':camlumo.values(),'BHandHYLP':bhandlumo.values(),'PBE1PBE':pbepbelumo.values()} 
+    df = pd.DataFrame(data=df)
+    df_2 = pd.DataFrame(data=df_2)
+    df.to_csv('/Users/tsantaloci/Desktop/python_projects/austin/Dyes/data_analysis/PiecesHOMO_B.csv',index=False)
+    df_2.to_csv('/Users/tsantaloci/Desktop/python_projects/austin/Dyes/data_analysis/PiecesLUMO_B.csv',index=False)
+    print(df) 
+    print(df_2)
+
+
 
     return    
 Main()
