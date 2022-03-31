aa7d1ff0ac31262e7ece61da776775180873a9fc
Austin Wallace <austinwallace196@gmail.com>
2022-03-31 12:31:32 -0500

    improved excitation generation for lsf

--- src/lsf_results.py
+++ src/lsf_results.py
@@ -1,11 +1,107 @@
 import molecule_json
 
 
+class lsfExcGather():
+    """ docstring for lsfExcGather."""
+
+    def __init__(self, v_abs, v_homo, v_lumo, v_osci):
+        self.v_abs = v_abs
+        self.v_homo = v_homo
+        self.v_lumo = v_lumo
+        self.v_osci = v_osci
+
+    def getDict(self):
+        return {
+            "v_abs": self.v_abs,
+            "v_homo": self.v_homo,
+            "v_lumo": self.v_lumo,
+            "v_osci": self.v_osci,
+        }
+
+
 def conv_energy(val, rounding=3):
     h = 6.626e-34
     c = 2.998e17
     J_over_eV = 1.602e-19
     return round(h * c / (float(val) * J_over_eV), rounding)
+
+
+def generate_lsf_exc(results_json="../Benchmark/benchmarks_exc.json"):
+    moleculeList = molecule_json.MoleculeList_exc()
+    moleculeList.setData(results_json)
+    homo_lsf = [-0.36764963, -0.38964081]  # CAM - PBE
+    lumo_lsf = [-0.95798195, 0.93317353]  # CAM - PBE
+    nm_lsf = [1.24306358, -0.38355558]  # CAM - PBE
+
+    d = moleculeList.getMoleculeList()
+    for n, mol in enumerate(d):
+        excs = mol.getExcitations()
+        # Gather cam and pbe0
+        cam = {}
+        pbe0 = {}
+
+        for ex in excs:
+            tmp_dict = ex.toDict()
+            v_abs = tmp_dict["nm"]
+            v_homo = tmp_dict["HOMO"]
+            v_lumo = tmp_dict["LUMO"]
+            v_osci = tmp_dict["osci"]
+            p = tmp_dict["exc"]
+            if (tmp_dict["method_basis_set"] == "CAM-B3LYP/6-311G(d,p)"):
+                cam["exc%d" % p] = lsfExcGather(v_abs, v_homo, v_lumo, v_osci)
+
+            if (tmp_dict["method_basis_set"] == "PBE1PBE/6-311G(d,p)"):
+                pbe0["exc%d" % p] = lsfExcGather(v_abs, v_homo, v_lumo, v_osci)
+
+                # do coefficient math...
+        for e in cam.keys():
+            if e in pbe0:
+                c_d = cam[e].getDict()
+                cam_h = c_d["v_homo"]
+                cam_l = c_d["v_lumo"]
+                cam_o = c_d["v_osci"]
+                cam_nm = c_d['v_abs']
+                p_d = cam[e].getDict()
+                pbe0_h = p_d["v_homo"]
+                pbe0_l = p_d["v_lumo"]
+                pbe0_o = c_d["v_osci"]
+                pbe0_nm = p_d['v_abs']
+                print(c_d, p_d)
+                homo = homo_lsf[0] * cam_h + homo_lsf[1] * pbe0_h
+                lumo = lumo_lsf[0] * cam_l + lumo_lsf[1] * pbe0_l
+                nm = nm_lsf[0] * conv_energy(cam_nm) + nm_lsf[1] * conv_energy(
+                    pbe0_nm)
+                nm = conv_energy(nm)
+                osci = nm_lsf[0] * cam_o + nm_lsf[1] * pbe0_o
+                new_d = {
+                    "exc":
+                    e[-1],
+                    "method_basis_set":
+                    "LSF",
+                    "nm":
+                    nm,
+                    "osci":
+                    osci,
+                    "orbital_Numbers": [
+                        # put coefficients here as an array of floats
+                        homo_lsf,
+                        lumo_lsf,
+                        nm_lsf
+                    ],
+                    "LUMO":
+                    homo,
+                    "HOMO":
+                    lumo,
+                }
+                new_exc = molecule_json.Excitation_exc()
+                new_exc.giveData(new_d)
+                mol.appendExcitations([new_exc])
+        d[n] = mol
+    moleculeList.setMolecules(d)
+    moleculeList.sendToFile("../json_files/test.json")
+
+    return
+
 
 def main():
     moleculeList = molecule_json.MoleculeList_exc()
@@ -85,4 +181,5 @@ def main():
     moleculeList.sendToFile("../json_files/test.json")
 
 
-main()
+# main()
+generate_lsf_exc()
