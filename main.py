bb259ce9b78b26b6b099cce16e8bda777ec18b16
Austin Wallace <austinwallace196@gmail.com>
2022-06-06 18:11:08 -0500

    absorption plot

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import json
 import src
 from src.molecule_json import Molecule_exc_to_db
+import pickle
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def write_pickle(data, fname='mol.pickle'):
+    with open(fname, 'wb') as handle:
+        pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+def read_pickle(fname='mol.pickle'):
+    with open(fname, 'rb') as handle:
+        return pickle.load(handle)
+
 
 # def user(x, y):
 #     filename = open(x + "/" + "user", "w+")
@@ -155,12 +169,112 @@ from src.molecule_json import Molecule_exc_to_db
 #         src.qmgr(config["qmgr"])
 #     return
 
+# def json_to_db():
+#     Molecule_exc_to_db('./json_files/test.json',
+#                        "./json_files/test2.json")
 
-def json_to_db():
-    Molecule_exc_to_db('./json_files/test.json',
-                       "./json_files/test2.json")
+
+def method_name_converter(method: str):
+    v = ""
+    c = ""
+    if method == 'lsf':
+        v = "LSF"
+        c = "green"
+    elif method == "CAM_B3LYP_6_311G_d_p":
+        v = "CAM-B3LYP/6-311G(d,p)"
+        c = "blue"
+    elif method == "PBE1PBE_6_311G_d_p":
+        v = "PBE1PBE/6-311G(d,p)"
+        c = "orange"
+    elif method == "bhandhlyp_6_311G_d_p":
+        v = "BHandHLYP/6-311G(d,p)"
+        c = "red"
+    else:
+        print("Method not supported ")
+    return v, c
+
+
+def convert_units(v):
+    h = 6.626e-34
+    c = 3e17
+    Joules_to_eV = 1.602e-19
+    v = h * c / (v * Joules_to_eV)
+    return v
+
+
+def plot_absoprtion_lambda_max(pickle_path="./pickles/ds_all5.pickle",
+                               plot_methods=[
+                                   "CAM_B3LYP_6_311G_d_p",
+                                   "PBE1PBE_6_311G_d_p",
+                                   "bhandhlyp_6_311G_d_p",
+                                   "lsf",
+                               ],
+                               sort_by="lsf",
+                               units="nm",
+                               output="theory_abs.png",
+                               transparent=True):
+    """
+    Plots the theoretical absorption data with filtering incomplete
+    excitation data or egregiously incorrect predictions
+    """
+    data = read_pickle(pickle_path)
+    plot_dict = {}
+    for i in plot_methods:
+        plot_dict[i] = []
+    for k, v in data.items():
+        for i in plot_methods:
+            if i in v:
+                e = -1
+                for j in v[i]:
+                    if j["exc"] == 1:
+                        e = j["nm"]
+                        break
+                if e != -1:
+                    if units == "eV":
+                        e = convert_units(e)
+                    plot_dict[i].append(e)
+                elif i == sort_by:
+                    plot_dict[i].append(0)
+                else:
+                    plot_dict[i].append(None)
+            elif i == sort_by:
+                plot_dict[i].append(0)
+            else:
+                plot_dict[i].append(None)
+    df = pd.DataFrame(plot_dict)
+    df = df.sort_values(sort_by)
+
+    # This filters out dyes with incomplete excitation data
+    df = df[df[sort_by] != 0]
+    # This filters bad excitations predictions
+    df = df[df[sort_by] < 1500]
+
+    df[sort_by].dropna()
+    xs = range(len(df[sort_by]))
+    for k in plot_methods:
+        label, color = method_name_converter(k)
+        plt.plot(xs, df[k], color=color, label=label, linewidth=0.5)
+    plt.xlabel("Theoretical Dyes")
+    plt.ylabel("Excitation Energy (%s)" % units)
+    plt.grid(color="grey",
+             which="major",
+             axis="y",
+             linestyle="-",
+             linewidth=0.2)
+    plt.legend()
+    out_path = "data_analysis/%s" % (output)
+    plt.savefig(out_path, transparent=transparent)
+    plt.show()
+    return
+
+
+def main():
+    plot_absoprtion_lambda_max()
+
+    return
 
 
 if __name__ == "__main__":
     # main()
-    json_to_db()
+    # json_to_db()
+    main()
